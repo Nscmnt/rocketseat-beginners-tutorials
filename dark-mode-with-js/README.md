0bf1f0b12a0a31bf193e9f5bd83b1dc50f17ea83
Jorge Nascimento <jorg3nascimento@gmail.com>
2021-04-07 13:18:00 -0300

    first commit for darkMode

---
+++ dark-mode-with-js/README.md
@@ -0,0 +1,7 @@
+# <center> DarkMode com JavaScript </center>
+
+DarkMode utilizando JavaScript com manipulação de variaveis CSS, vídeo utilizado do tutorial - [_Dark Mode raiz com CSS e JavaScript | Code/Drops #24_](https://www.youtube.com/watch?v=BvhYm0BOLvA&list=PL85ITvJ7FLoifcDIBeuuAhh4_799RZaSc&index=23&ab_channel=Rocketseat)
+
+</br> <hr>
+
+![darkMode gif](./darkMode.gif)
---
+++ dark-mode-with-js/scripts.js
@@ -0,0 +1,34 @@
+const html = document.querySelector("html");
+
+const checkbox = document.querySelector("input[name=theme");
+
+const getStyle = (element, style) => {
+  return window.getComputedStyle(element).getPropertyValue(style);
+};
+
+const initialsColors = {
+  bg: getStyle(html, "--bg"),
+  bgPanel: getStyle(html, "--bg-panel"),
+  colorHeading: getStyle(html, "--color-headings"),
+  colorText: getStyle(html, "--color-text"),
+};
+
+const darkMode = {
+  bg: "#333333",
+  bgPanel: "#434343",
+  colorHeading: "#3664FF",
+  colorText: "#B5B5B5",
+};
+
+const transformKey = (key) =>
+  "--" + key.replace(/([A-Z])/, "-$1").toLowerCase();
+
+const changeColors = (colors) => {
+  Object.keys(colors).map((key) => {
+    html.style.setProperty(transformKey(key), colors[key]);
+  });
+};
+
+checkbox.addEventListener("change", ({ target }) => {
+  target.checked ? changeColors(darkMode) : changeColors(initialsColors);
+});
