9bffd63ec8c278dc4933b9d48398645a3e69eebc
Jorge Nascimento <jorg3nascimento@gmail.com>
2021-04-18 13:59:10 -0300

    finalizando o tutorial cosumindo api

---
+++ consuming-api/README.md
@@ -0,0 +1,7 @@
+# <center> Consumindo API no front e Back end </center>
+
+Consumindo API criada no backend que essa condume outra api para buscar usuários fakes JSONPlaceholder, seguindo tutorial do vídeo - [_Consumindo API no Front e no Back-end | CodeDrops #45_](https://www.youtube.com/watch?v=vYlz3SmNXQQ&list=PL85ITvJ7FLoifcDIBeuuAhh4_799RZaSc&index=12&ab_channel=Rocketseat)
+
+</br> <hr>
+
+![buscar usuario gif](./.github/buscar.gif)
---
+++ consuming-api/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+
+    return res.json(data);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.listen("4567", () => console.log("running"));
